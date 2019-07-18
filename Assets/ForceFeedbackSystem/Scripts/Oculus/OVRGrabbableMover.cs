a5d0d94aa6ac77e645acaae6f23a716c2c3a6dbf
Sébastien Poivre <gizmhail@gmail.com>
2019-07-19 00:53:19 +0200

    Add missing Oculus implementation files

---
+++ Assets/ForceFeedbackSystem/Scripts/Oculus/OVRGrabbableMover.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ForceFeedbackSystem
+{
+    public class OVRGrabbableMover : OVRGrabbable, IIRLMover, IHapticActioner
+    {
+        IIRLMoveHandler _moveHandler;
+        IIRLMoveHandler MoveHandler
+        {
+            get
+            {
+                if (_moveHandler == null) _moveHandler = GetComponent<IIRLMoveHandler>();
+                return _moveHandler;
+            }
+        }
+        public bool moving = false;
+        public OVRInput.Controller grabbingControllerKind = OVRInput.Controller.None;
+        float lastVibrationLevel = 0;
+
+        [Header("Force feedback system")]
+        public bool installForceFeedbackSystem = true;
+        public Material ghostMaterial;
+        public string irlLayerName = "RealLifeVR";
+        public string vrWorldLayerName = "GameVR";
+        public string grabbingIrlLayerName = "GrabbingRealLife";
+
+        [Header("Set dynamically")]
+        public VRForceFeedbackSystem forceFeedbackSystem;
+        public Vector3 grabSpotLocalPosition;
+        public Quaternion grabSpotLocalRotation;
+
+        private void Awake()
+        {
+            
+            if (m_grabPoints.Length == 0)
+            {
+                // Get the collider from the grabbable
+                Collider collider = this.GetComponent<Collider>();
+                if (collider == null)
+                {
+                    throw new ArgumentException("Grabbables cannot have zero grab points and no collider -- please add a grab point or collider.");
+                }
+
+                // Create a default grab point
+                m_grabPoints = new Collider[1] { collider };
+            }
+        }
+
+        protected override void Start()
+        {
+            base.Start();
+            if (installForceFeedbackSystem && GetComponent<VRForceFeedbackSystem>() == null)
+            {
+                forceFeedbackSystem = gameObject.AddComponent<VRForceFeedbackSystem>();
+                forceFeedbackSystem.ghostMaterial = ghostMaterial;
+                forceFeedbackSystem.irlLayerName = irlLayerName;
+                forceFeedbackSystem.vrWorldLayerName = vrWorldLayerName;
+            }
+            else
+            {
+                forceFeedbackSystem = GetComponent<VRForceFeedbackSystem>();
+            }
+        }
+
+        private void Update()
+        {
+            if (MoveHandler != null && moving)
+            {
+                MoveHandler.OnIRLMove(gameObject);
+            }
+        }
+
+        public override void GrabBegin(OVRGrabber hand, Collider grabPoint)
+        {
+            base.GrabBegin(hand, grabPoint);
+
+            if(hand.gameObject.layer != LayerMask.NameToLayer(grabbingIrlLayerName))
+            {
+                Debug.LogError("To behave properly, the grabbing object should be in a dedicated layer, the only one interacting with the " + irlLayerName + " layer");
+            }
+
+            grabSpotLocalPosition = transform.InverseTransformPoint(grabbedBy.transform.position);
+            grabSpotLocalRotation = Quaternion.Inverse(transform.rotation) * grabbedBy.transform.rotation;
+
+            DetectGrabbingController();
+            if (MoveHandler != null)
+            {
+                MoveHandler.OnIRLMoveStart(gameObject, grabSpotLocalPosition, grabSpotLocalRotation);
+                moving = true;
+            }
+
+            // warn VR hands
+            if (grabbingControllerKind == OVRInput.Controller.LTouch && OVRVRHand.Left != null)
+            {
+                OVRVRHand.Left.grabbedObject = this;
+            }
+            if (grabbingControllerKind == OVRInput.Controller.RTouch && OVRVRHand.Right != null)
+            {
+                OVRVRHand.Right.grabbedObject = this;
+            }
+        }
+
+        public override void GrabEnd(Vector3 linearVelocity, Vector3 angularVelocity)
+        {
+
+            base.GrabEnd(linearVelocity, angularVelocity);
+            if (MoveHandler != null)
+            {
+                MoveHandler.OnIRLMoveEnd(gameObject);
+                moving = false;
+            }
+        }
+
+        #region Haptic 
+
+        void DetectGrabbingController()
+        {
+            //Note: instead of parsing name, we could subclass OVRGrabber to make the controller kind public
+            grabbingControllerKind = OVRInput.Controller.None;
+            if (grabbedBy != null)
+            {
+                if (grabbedBy.name.Contains("Left"))
+                {
+                    grabbingControllerKind = OVRInput.Controller.LTouch;
+                }
+                if (grabbedBy.name.Contains("Right"))
+                {
+                    grabbingControllerKind = OVRInput.Controller.RTouch;
+                }
+            }
+        }
+
+        public void SetVibrationLevel(float level)
+        {
+            if (grabbingControllerKind == OVRInput.Controller.None) return;
+            if (level == 0 && lastVibrationLevel == 0) return;
+            lastVibrationLevel = level;
+
+            if (level == 0)
+            {
+                OVRInput.SetControllerVibration(0, 0, grabbingControllerKind);
+            }
+            else
+            {
+                OVRInput.SetControllerVibration(frequency: level, amplitude: level, grabbingControllerKind);
+            }
+        }
+        #endregion
+    }
+}
+
---
+++ Assets/ForceFeedbackSystem/Scripts/Oculus/OVRVRHand.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ForceFeedbackSystem
+{
+    public class OVRVRHand : MonoBehaviour
+    {
+        public OVRInput.Controller controllerKind;
+        public Material ghostHandMaterial;
+        public bool displayCopyHand = true;
+
+        [Header("Set dynamically")]
+        public GameObject originalHand;
+        public GameObject copyHand;
+        public Transform[] handParts;
+
+        public OVRGrabbableMover grabbedObject;
+        public SkinnedMeshRenderer originalHandMesh;
+        public Material originalHandMaterial;
+        public bool usingGhostMaterial;
+        public static OVRVRHand Left;
+        public static OVRVRHand Right;
+
+        void InitializeCopyHand()
+        {
+            if (copyHand != null) return;
+            if (controllerKind == OVRInput.Controller.LTouch)
+            {
+                OVRVRHand.Left = this;
+                originalHand = GameObject.Find("hand_left_renderPart_0");
+            }
+            if (controllerKind == OVRInput.Controller.RTouch)
+            {
+                OVRVRHand.Right = this;
+                originalHand = GameObject.Find("hand_right_renderPart_0");
+            }
+            if (originalHand == null) return;
+            originalHandMesh = originalHand.GetComponent<SkinnedMeshRenderer>();
+            if (originalHandMesh) originalHandMaterial = originalHandMesh.material;
+            copyHand = Object.Instantiate(originalHand);
+            copyHand.transform.parent = transform;
+            copyHand.SetActive(false);
+            Transform[] originalHandParts = originalHand.GetComponentsInChildren<Transform>(true);
+            Transform[] copyHandParts = copyHand.GetComponentsInChildren<Transform>(true);
+            for (int i = 0; i < copyHandParts.Length; i++)
+            {
+                var original = originalHandParts[i].gameObject;
+                var copy = copyHandParts[i].gameObject;
+                if (copy != copyHand)
+                {
+                    copy.SetActive(true);
+                    var mirror = copy.AddComponent<MirrorLocalTransform>();
+                    mirror.target = original.transform;
+                }
+
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            InitializeCopyHand();
+
+            // Check grabbed object
+            if (!grabbedObject) return;
+            if (!grabbedObject.isGrabbed || grabbedObject.grabbingControllerKind != controllerKind)
+            {
+                grabbedObject = null;
+                copyHand.SetActive(false);
+            }
+            else
+            {
+                copyHand.SetActive(displayCopyHand);
+            }
+
+            if (!grabbedObject) return;
+
+            bool displayVRHandNeeded = grabbedObject.forceFeedbackSystem.status == VRForceFeedbackSystem.FFSStatus.TryingSynchronisation
+                || grabbedObject.forceFeedbackSystem.status == VRForceFeedbackSystem.FFSStatus.JoiningBack;
+            
+            // Enable physics world hand
+            copyHand.SetActive(displayCopyHand && displayVRHandNeeded);
+
+            // Track physics object
+            copyHand.transform.position = grabbedObject.forceFeedbackSystem.vrWorldObject.transform.TransformPoint(grabbedObject.grabSpotLocalPosition);
+            copyHand.transform.rotation = grabbedObject.forceFeedbackSystem.vrWorldObject.transform.rotation * grabbedObject.grabSpotLocalRotation;
+            Debug.Log("k:" + grabbedObject.grabSpotLocalPosition);
+
+            // Change real avatar hand material
+            if (displayVRHandNeeded && ghostHandMaterial != null)
+            {
+                originalHandMesh.material = ghostHandMaterial;
+                usingGhostMaterial = true;
+            }
+            else if (usingGhostMaterial)
+            {
+                usingGhostMaterial = false;
+                originalHandMesh.material = originalHandMaterial;
+            }
+        }
+    } 
+}
